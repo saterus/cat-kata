9b58a2c4545cc9cdf200579ffaaf0d09cb18a57a
Alex Burkhart <saterus@gmail.com>
2015-10-28 20:19:52 -0400

    Read a single file and output contents to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+use std::env::args;
+use std::fs::File;
+use std::io::Read;
+
+#[allow(dead_code)]
 static HELP_TEXT: &'static str = "usage: cat [-benstuv] [file ...]";
 
 fn main() {
-    print_usage();
+    let path = args().skip(1).take(1).last().expect("You must pass a path as the first argument.");
+    let mut file = File::open(path).unwrap();
+    let mut buffer = String::new();
+    file.read_to_string(&mut buffer).unwrap();
+    print!("{}", buffer);
 }
 
-fn print_usage() {
-    println!("{}", HELP_TEXT);
-}
+
